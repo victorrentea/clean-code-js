741ce6f07dba341f87db2ab3a3b5470fcb17d867
Victor Rentea <victorrentea@gmail.com>
2023-09-22 11:36:23 +0300

    flattened the function a bit

--- src/booleans-boss.js
+++ src/booleans-boss.js
@@ -1,28 +1,28 @@
-function bossLevel(fluff, tasks, cr323) {
+function bossLevel(tasks, cr323) {
     let index = 0;
     let taskCount = tasks.length;
     console.log("Logic1");
     let taskIds = [];
-    if (fluff) {
-        console.log("Logic3");
-        for (let task of tasks) {
-            console.log("Starting " + task);
-            task.started = true;
+    console.log("Logic3");
+    for (let task of tasks) {
+        console.log("Starting " + task);
+        task.started = true;
 
-            taskIds.push(task.id);
+        taskIds.push(task.id);
 
-            if (cr323) { // TODO task = remove the boolean
-                console.log("My Logic: " + task);
-            }
-
-            index++;
-            console.log("Audit task #" + index + ": " + task);
+        if (cr323) { // TODO task = remove the boolean
+            console.log("My Logic: " + task);
         }
-        console.log("Logic6 " + taskCount);
-        console.log("Task Ids: " + taskIds);
-    } else {
-        console.log("Logic7 on fluff=false " + tasks);
+
+        index++;
+        console.log("Audit task #" + index + ": " + task);
     }
+    console.log("Logic6 " + taskCount);
+    console.log("Task Ids: " + taskIds);
+    console.log("Logic8");
+}function bossLevelNoFluff(tasks) {
+    console.log("Logic1");
+    console.log("Logic7 on fluff=false " + tasks);
     console.log("Logic8");
 }
 
@@ -35,14 +35,14 @@ class Task {
 }
 
 console.log("== 1 ==")
-bossLevel(true, [new Task(5)], false);
+bossLevel([new Task(5)], false);
 console.log("== 2 ==")
-bossLevel(true, [new Task(5)], false);
+bossLevel([new Task(5)], false);
 console.log("== 3 ==")
-bossLevel(true, [new Task(5)], true);
+bossLevel([new Task(5)], true);
 console.log("== 4 ==")
-bossLevel(true, [], false);
+bossLevel([], false);
 console.log("== 5 ==")
-bossLevel(false, [new Task(5)], false);
+bossLevelNoFluff([new Task(5)]);
 console.log("== 6 ==")
-bossLevel(true, [new Task(5),new Task(6)], false);
+bossLevel([new Task(5), new Task(6)], false);
