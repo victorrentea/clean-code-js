5060453bc8d7f3175859cde84d310a43b227ffeb
Victor Rentea <victorrentea@gmail.com>
2023-09-22 17:19:36 +0300

    using clojures TO HIDE AWAY STATE. Victor hates it.

---
+++ src/kata-video-store.spec.js
@@ -0,0 +1,26 @@
+import assert from 'chai';
+import {statement} from "./kata-video-store.js";
+
+describe("aa", () => {
+    it("stuff", () => {
+        const customer =
+            {
+                name: "martin",
+                rentals: [
+                    {movie: {title: "Star Wars", code: "new"}, days: 6},
+                    {movie: {title: "Sofia", code: "childrens"}, days: 7},
+                    {movie: {title: "Inception", code: "regular"}, days: 5}
+                ]
+            }
+        ;
+        const expected = `Rental Record for martin
+\tStar Wars\t18
+\tSofia\t7.5
+\tInception\t6.5
+Amount owed is 32
+You earned 4 frequent renter points
+`;
+        const actual = statement(customer);
+        assert.expect(actual).to.be.eq(expected)
+    });
+});
