7bf808562ee6ca81825eed71e6e5cf7ecfa3b085
Victor Rentea <victorrentea@gmail.com>
2023-09-22 16:45:38 +0300

    te code should be aligned to what the requirement state.

--- src/kata-video-store.js
+++ src/kata-video-store.js
@@ -1,43 +1,47 @@
 
 export function statement(customer) {
-    let totalAmount = 0;
+    let totalPrice = 0;
     let frequentRenterPoints = 0;
     let result = `Rental Record for ${customer.name}\n`;
-    for (let r of customer.rentals) {
-        let movie = r.movie;
-        let thisAmount = 0;
-
-        // determine amount for each movie
-        switch (movie.code) {
-            case "regular":
-                thisAmount += 2;
-                if (r.d > 2) {
-                    thisAmount += (r.d - 2) * 1.5;
-                }
-                break;
-            case "new":
-                thisAmount = r.d * 3;
-                break;
-            case "childrens":
-                thisAmount += 1.5;
-                if (r.d > 3) {
-                    thisAmount += (r.d - 3) * 1.5;
-                }
-                break;
-        }
+    for (let rental of customer.rentals) {
+        let movie = rental.movie;
+        
+        const price = calculatePrice(movie, rental);
 
         //add frequent renter points
         frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if (movie.code === "new" && r.d > 2) frequentRenterPoints++;
+        // add bonus for a 3 day new release rental
+        if (movie.code === "new" && rental.d >= 3) frequentRenterPoints++;
 
         //print figures for this rental
-        result += `\t${movie.title}\t${thisAmount}\n`;
-        totalAmount += thisAmount;
+        result += `\t${movie.title}\t${price}\n`;
+        totalPrice += price;
     }
     // add footer lines
-    result += `Amount owed is ${totalAmount}\n`;
+    result += `Amount owed is ${totalPrice}\n`;
     result += `You earned ${frequentRenterPoints} frequent renter points\n`;
 
     return result;
 }
+
+function calculatePrice(movie, rental) {
+    let price = 0;
+    switch (movie.code) {
+        case "regular":
+            price += 2;
+            if (rental.d > 2) // TODO what the heck is "d"
+                price += (rental.d - 2) * 1.5;
+            break;
+        case "new":
+            price = rental.d * 3;
+            break;
+        case "childrens":
+            price += 1.5;
+            if (rental.d > 3) {
+                price += (rental.d - 3) * 1.5;
+            }
+            break;
+    }
+    return price;
+}
+
