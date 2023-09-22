3b6e468f217f59bb5a0c2df30860772d42c60ad7
Victor Rentea <victorrentea@gmail.com>
2023-09-22 17:21:58 +0300

    += function()

--- src/kata-video-store.js
+++ src/kata-video-store.js
@@ -1,37 +1,39 @@
 export function statement(customer) {
     let totalPrice = 0;
-    // let frequentRenterPoints = 0;
+    let frequentRenterPoints = 0;
     let result = `Rental Record for ${customer.name}\n`;
-    const f = addRenterPoints();
     for (const rental of customer.rentals) {
         const price = calculatePrice(rental);
-
-        f.accumulate(rental);
-
+        frequentRenterPoints += calculateRenterPoints(rental);
         //print figures for this rental
         result += `\t${rental.movie.title}\t${price}\n`;
         totalPrice += price;
     }
     // add footer lines
     result += `Amount owed is ${totalPrice}\n`;
-    result += `You earned ${f.getTotal()} frequent renter points\n`;
+    result += `You earned ${frequentRenterPoints} frequent renter points\n`;
 
     return result;
 }
 
+function calculateRenterPoints(rental) {
+    let frequentRentalPoints = 1;
+    if (deservesBonus(rental)) frequentRentalPoints++;
+    return frequentRentalPoints;
+}
 
-const addRenterPoints = () => {
-    let frequentRentalPoints = 0;
-
-    return {
-        accumulate: (rental) => {
-            frequentRentalPoints += 1;
-            if (deservesBonus(rental)) frequentRentalPoints++;
-            return frequentRentalPoints;
-        },
-        getTotal: () => frequentRentalPoints
-    }
-};
+// const addRenterPoints = () => {
+//     let frequentRentalPoints = 0;
+//
+//     return {
+//         accumulate: (rental) => {
+//             frequentRentalPoints += 1;
+//             if (deservesBonus(rental)) frequentRentalPoints++;
+//             return frequentRentalPoints;
+//         },
+//         getTotal: () => frequentRentalPoints
+//     }
+// };
 
 function deservesBonus(rental) {
     return rental.movie.code === "new" && rental.days >= 3;
